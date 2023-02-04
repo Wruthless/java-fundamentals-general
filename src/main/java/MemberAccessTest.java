75e3af4cede53689c430c49f18d8089f555256a2
Wruthless <wsgnzls@comcast.net>
2023-02-04 12:23:23 -0500

    Added MemberAccessTest.java, short example of private fields.

---
+++ src/main/java/MemberAccessTest.java
@@ -0,0 +1,11 @@
+public class MemberAccessTest {
+
+    public static void main(String[] args) {
+        Time1 time1 = new Time1();
+
+        // private members cannot be directly access instance variables.
+        time1.hour = 7;
+        time1.minute = 15;
+        time1.second = 30;
+    }
+}
