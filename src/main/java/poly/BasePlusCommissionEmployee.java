222bbdfdd162b339e0cf77eb880a1f70acfe1831
Wruthless <wsgnzls@comcast.net>
2023-02-22 06:09:45 -0500

    More polymorphic examples, update to README.md.

---
+++ src/main/java/poly/BasePlusCommissionEmployee.java
@@ -0,0 +1,35 @@
+package poly;
+
+public class BasePlusCommissionEmployee extends CommissionEmployee{
+
+    private double baseSalary;
+
+    public BasePlusCommissionEmployee(String firstName, String lastName, String ssn,
+                                      double grossSales, double commissionRate, double baseSalary) {
+
+        // CommissionEmployee constructor
+        super(firstName, lastName, ssn, grossSales, commissionRate);
+        this.baseSalary = baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    @Override
+    public double earnings() {
+        return getBaseSalary() + super.earnings();
+    }
+
+    @Override
+    public String toString() {
+        return "BasePlusCommissionEmployee{" +
+            "super=" + super.toString() + " " +
+            "baseSalary=" + baseSalary +
+            '}';
+    }
+}
