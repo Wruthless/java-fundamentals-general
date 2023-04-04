bfcc9bc83b0bad73991e4b2e6bc9a032fd84af9e
Wruthless <wsgnzls@comcast.net>
2023-04-04 10:33:42 -0400

    Some more complex stream examples for custom data types.

--- src/main/java/lambdasStreams/ProcessingEmployees.java
+++ src/main/java/lambdasStreams/ProcessingEmployees.java
@@ -1,8 +1,6 @@
 package lambdasStreams;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -11,14 +9,10 @@ public class ProcessingEmployees {
 
     public static void main(String[] args) {
 
-        Employee[] employees = {
-            new Employee("Jason", "Red", 5000, "IT"),
-            new Employee("Ashley", "Green", 7600, "IT"),
-            new Employee("James", "Indigo", 3857.5, "Sales"),
-            new Employee("Luke", "Indigo", 4700.77, "Marketing"),
-            new Employee("Matthew", "Indigo", 6200, "IT"),
-            new Employee("Jason", "Blue", 3200, "Sales"),
-            new Employee("Wendy", "Brown", 4236.4, "Marketing")};
+        Employee[] employees = { new Employee("Jason", "Red", 5000, "IT"), new Employee("Ashley", "Green", 7600,
+            "IT"), new Employee("James", "Indigo", 3857.5, "Sales"), new Employee("Luke", "Indigo", 4700.77,
+            "Marketing"), new Employee("Matthew", "Indigo", 6200, "IT"), new Employee("Jason", "Blue", 3200, "Sales")
+            , new Employee("Wendy", "Brown", 4236.4, "Marketing") };
 
         // get list view of the Employees
         List<Employee> list = Arrays.asList(employees);
@@ -29,21 +23,15 @@ public class ProcessingEmployees {
 
         // Predicate that return true for salaries between 4 and 6k
         Predicate<Employee> fourToSixThousand =
-            employee -> (employee.getSalary() >= 4000 && employee.getSalary() <= 6000);
+            employee -> ( employee.getSalary() >= 4000 && employee.getSalary() <= 6000 );
 
         System.out.printf("%nEmployees earning between 4 and 6k sorted by salary:%n");
-        list.stream()
-            .filter(fourToSixThousand)
-            .sorted(Comparator.comparing(Employee::getSalary))
-            .forEach(System.out::println);
+        list.stream().filter(fourToSixThousand).sorted(Comparator.comparing(Employee::getSalary)).forEach(System.out::println);
 
 
         // Display first Employee with a salary between $4-6k
         System.out.printf("%nFirst employee who earns between $4-6k: %n%s%n",
-            list.stream()
-                .filter(fourToSixThousand)
-                .findFirst()
-                .get());
+            list.stream().filter(fourToSixThousand).findFirst().get());
 
         // Get first and last name
         Function<Employee, String> byFirstName = Employee::getFirstName;
@@ -54,29 +42,35 @@ public class ProcessingEmployees {
 
         // sort by last then first name
         System.out.printf("%nEmployee in asc order by last then first name: %n");
-            list.stream()
-                .sorted(lastThenFirst)
-                .forEach(System.out::println);
+        list.stream().sorted(lastThenFirst).forEach(System.out::println);
 
         System.out.printf("%nEmployees in desc order by last then first name: %n");
-        list.stream()
-            .sorted(lastThenFirst.reversed())
-            .forEach(System.out::println);
+        list.stream().sorted(lastThenFirst.reversed()).forEach(System.out::println);
 
         // unique employees sorted by last name
         System.out.printf("%nUnique employees by last name: %n");
-        list.stream()
-            .map(Employee::getLastName)
-            .distinct()
-            .sorted()
-            .forEach(System.out::println);
+        list.stream().map(Employee::getLastName).distinct().sorted().forEach(System.out::println);
 
         // display first and last
         System.out.printf("%nEmployee names in order by last then first name: %n");
-        list.stream()
-            .sorted(lastThenFirst)
-            .map(Employee::getName)
-            .forEach(System.out::println);
+        list.stream().sorted(lastThenFirst).map(Employee::getName).forEach(System.out::println);
 
+
+        // Group Employees by department
+        System.out.printf("%nEmployee by department: %n");
+        Map<String, List<Employee>> groupedByDepartment =
+            list.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+
+        groupedByDepartment.forEach((department, employeesInDepartment) -> {
+            System.out.println(department);
+            employeesInDepartment.forEach(employee -> System.out.printf("   %s%n", employee));
+        });
+
+        // number of employees in each department
+        System.out.printf("%nNumber of employees by department: %n");
+        Map<String, Long> employeeCountByDepartment =
+            list.stream().collect(Collectors.groupingBy(Employee::getDepartment, TreeMap::new, Collectors.counting()));
+
+        employeeCountByDepartment.forEach(( department, count) -> System.out.printf("%s has %d employee(s)%n", department, count));
     }
 }
